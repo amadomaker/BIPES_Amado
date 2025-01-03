2e4f2d51b03b549112999446b0ce97b1fd81ea21
Antonio Mortari <antoniomortari09@gmail.com>
2025-01-03 12:38:28 -0300

    adicionado a tradução da categoria machine

--- ui/msg/pt-br.js
+++ ui/msg/pt-br.js
@@ -334,5 +334,6 @@ Blockly.Msg['CAT_BUZZER'] = "Campainha";
 Blockly.Msg['CAT_WIFI'] = "WiFi";
 Blockly.Msg['CAT_HTTPCLIENT'] = "Cliente HTTP";
 Blockly.Msg['CAT_HTTPSERVER'] = "Servidor HTTP"
+Blockly.Msg['CAT_MACHINE'] = "Máquina";
 
 
