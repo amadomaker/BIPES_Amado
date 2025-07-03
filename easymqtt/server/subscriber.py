9488847a49f50e2b0d6008b68b8b6660b9142b61
Vinicius Costa <costavinicius692@gmail.com>
2025-07-03 11:18:39 -0300

    fix: corrige erro na conexao com o mqtt

--- easymqtt/server/subscriber.py
+++ easymqtt/server/subscriber.py
@@ -4,12 +4,6 @@ import json
 import paho.mqtt.client as mqtt
 from pymongo import MongoClient
 
-# Associa callbacks
-mqtt_client.on_connect = mqtt_on_connect
-mqtt_client.on_message = mqtt_on_message
-mqtt_client.on_disconnect = mqtt_on_disconnect
-mqtt_client.on_subscribe = mqtt_on_subscribe
-
 # Configura credenciais
 mqtt_host = os.getenv("MQTT_HOST", "localhost")
 mqtt_user = os.getenv("MQTT_USER", "bipes")
@@ -89,6 +83,12 @@ def mqtt_on_disconnect(client, userdata, rc, properties=None):
 
 def mqtt_on_subscribe(client, userdata, mid, granted_qos, properties=None):
     print(f"Subscribed successfully with QoS {granted_qos}")
+
+# Associa callbacks
+mqtt_client.on_connect = mqtt_on_connect
+mqtt_client.on_message = mqtt_on_message
+mqtt_client.on_disconnect = mqtt_on_disconnect
+mqtt_client.on_subscribe = mqtt_on_subscribe
 
 mqtt_client.username_pw_set(mqtt_user, password=mqtt_pass)
 print("Callbacks set, will attempt connect")  # Debug
