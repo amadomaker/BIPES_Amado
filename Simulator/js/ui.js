07b6daf460300ab63e49c2771a2910add77baa83
joaodrj <junior44rpm@gmail.com>
2025-11-05 09:28:05 -0300

    Atualizando icones de rotação e inverter

--- Simulator/js/ui.js
+++ Simulator/js/ui.js
@@ -59,16 +59,14 @@ const TOOLBAR_ICON_SVGS = {
     </svg>
   `,
   rotate: `
-    <svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#1f2937" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-      <path d="M17 7a7 7 0 1 0-6 11"/>
-      <path d="M11 18H6l1.5-2.5Z" fill="#1f2937" stroke="#1f2937"/>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
     </svg>
+
   `,
   flip: `
-    <svg width="18" height="18" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="#1f2937" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round">
-      <path d="M6 6l3 6-3 6Z"/>
-      <path d="M18 6l-3 6 3 6Z" fill="#1f2937" stroke="#1f2937"/>
-      <path d="M12 4v16"/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+      <path d="M16 16V12L21 17L16 22V18H4V16H16ZM8 2V5.999L20 6V8H8V12L3 7L8 2Z"></path>
     </svg>
   `,
 };
